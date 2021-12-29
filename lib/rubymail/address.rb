7ca1607fdafb2b0c6461969a205203adc87e7dbb
krishpranav <krisna.pranav@gmail.com>
2021-12-29 18:39:26 +0530

    lib: rubymail address

--- lib/rubymail/address.rb
+++ lib/rubymail/address.rb
@@ -1,14 +1,17 @@
 module Rubymail
     class Address
-
-        def initialize(rubymail)
-            @mailgun = mailgun
-        end
-
-        def validate(email)
-            Rubymail.submit :get, address_url('validate'), {:address => email}
-        end
-
-        private
+      def initialize(rubymail)
+        @rubymail = rubymail
+      end
+  
+      def validate(email)
+        Rubymail.submit :get, address_url('validate'), {:address => email}
+      end
+  
+      private
+  
+      def address_url(action)
+        "#{@rubymail.public_base_url}/address/#{action}"
+      end
     end
-end
+  end
