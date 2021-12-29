81293e2d6204cec8913429f94489d481e7a43eb5
krishpranav <krisna.pranav@gmail.com>
2021-12-29 19:09:04 +0530

    lib: rubymail unsubscribe

--- lib/rubymail/unsubscribe.rb
+++ lib/rubymail/unsubscribe.rb
@@ -1,29 +1,31 @@
 module Rubymail
     class Unsubscribe
-        
-        def initialize(rubymail, domain)
-            @rubymail = rubymail
-            @domain = domain
-        end
-
-        def list(options={})
-            Rubymail.submit(:get, unsubscribe_url, options)["items"]
-        end
-
-        def find(email)
-            Rubymail.submit :get, unsubscribe_url(email)
-        end
-
-        def add(email, tag='*')
-            Rubymail.submit :post, unsubscribe_url, {:address => email, :tag => tag}
-        end
-
-        def remove(email)
-            Rubymail.submit :delete, unsubscribe_url(email)
-        end
-
-        private
-        
-        end
+      def initialize(rubymail, domain)
+        @rubymail = rubymail
+        @domain  = domain
+      end
+      
+      def list(options={})
+        Mailgun.submit(:get, unsubscribe_url, options)["items"]
+      end
+  
+      def find(email)
+        Mailgun.submit :get, unsubscribe_url(email)
+      end
+  
+      def add(email, tag='*')
+        Mailgun.submit :post, unsubscribe_url, {:address => email, :tag => tag}
+      end
+  
+      def remove(email)
+        Mailgun.submit :delete, unsubscribe_url(email)
+      end
+      
+      private
+  
+      def unsubscribe_url(address=nil)
+        "#{@rubymail.base_url}/#{@domain}/unsubscribes#{'/' + address if address}"
+      end
+      
     end
-end
+  end
