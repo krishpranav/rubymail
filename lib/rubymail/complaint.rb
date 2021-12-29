216832ff0db367557db963bb28b91013251e2103
krishpranav <krisna.pranav@gmail.com>
2021-12-29 19:45:37 +0530

    lib: rubymail complaint

--- lib/rubymail/complaint.rb
+++ lib/rubymail/complaint.rb
@@ -1,21 +1,31 @@
 module Rubymail
     class Complaint
-        def initialize(rubymail, domain)
-            @rubymail = rubymail
-            @domain = domain
-        end
-
-        def list(options={})
-            Rubymail.submit(:get, complaint_url, options)["items"] || []
-        end
-
-        def find(email)
-            Rubymail.submit :get, complaint_url(email)
-        end
-
-        def add(email)
-            Rubymail.submit :post, complaint_url, {:address => email}
-        end
-        
+      def initialize(rubymail, domain)
+        @rubymail = rubymail
+        @domain  = domain
+      end
+      
+      def list(options={})
+        Rubymail.submit(:get, complaint_url, options)["items"] || []
+      end
+  
+      def find(email)
+        Rubymail.submit :get, complaint_url(email)
+      end
+  
+      def add(email)
+        Rubymail.submit :post, complaint_url, {:address => email}
+      end
+  
+      def destroy(email)
+        Rubymail.submit :delete, complaint_url(email)
+      end
+  
+      private
+  
+      def complaint_url(address=nil)
+        "#{@rubymail.base_url}/#{@domain}/complaints#{'/' + address if address}"
+      end
+  
     end
-end
+  end
