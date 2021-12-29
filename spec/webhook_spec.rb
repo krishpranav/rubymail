481dcf738fc7c59dfdaa6ac3e809efcda388bcbc
krishpranav <krisna.pranav@gmail.com>
2021-12-29 19:03:19 +0530

    spec: webhook_spec

---
+++ spec/webhook_spec.rb
@@ -0,0 +1,115 @@
+require 'spec_helper'
+
+describe Rubymail::Webhook do
+
+  before :each do
+    @rubymail = Rubymail({:api_key => "api-key", :webhook_url => "http://postbin.heroku.com/860bcd65"})
+
+    @sample = {
+      :id     => "click",
+      :url    => "http://postbin.heroku.com/860bcd65"
+    }
+  end
+
+  describe "list avabilable webhook ids" do
+    it "should return the correct ids" do
+      expect(@rubymail.webhooks.available_ids).to match_array %i(bounce deliver drop spam unsubscribe click open)
+    end
+  end
+
+  describe "list webhooks" do
+    it "should make a GET request with the correct params" do
+
+      sample_response = "{\"total_count\": 1, \"items\": [{\"webhooks\":{\"open\":{\"url\":\"http://postbin.heroku.com/860bcd65\"},\"click\":{\"url\":\"http://postbin.heroku.com/860bcd65\"}}}]}"
+      webhooks_url = @rubymail.webhooks.send(:webhook_url)
+
+      expect(Rubymail).to receive(:submit).
+        with(:get, webhooks_url).
+        and_return(sample_response)
+
+      @rubymail.webhooks.list
+    end
+  end
+
+  describe "find a webhook" do
+    it "should make a GET request with correct params to find a given webhook" do
+      sample_response = "{\"webhook\": {\"url\":\"http://postbin.heroku.com/860bcd65\"}"
+      webhooks_url = @rubymail.webhooks.send(:webhook_url, @sample[:id])
+
+      expect(Rubymail).to receive(:submit).
+        with(:get, webhooks_url).
+        and_return(sample_response)
+
+      @rubymail.webhooks.find(@sample[:id])
+    end
+  end
+
+  describe "add a webhook" do
+    context "using the default webhook url" do
+      it "should make a POST request with correct params to add a webhook" do
+        sample_response = "{\"message\":\"Webhook has been created\",\"webhook\":{\"url\":\"http://postbin.heroku.com/860bcd65\"}}"
+        webhooks_url = @rubymail.webhooks.send(:webhook_url)
+
+        expect(Rubymail).to receive(:submit).
+          with(:post, webhooks_url, {:id => @sample[:id], :url => @sample[:url]}).
+          and_return(sample_response)
+
+        @rubymail.webhooks.create(@sample[:id])
+      end
+    end
+    context "overwriting the default webhook url" do
+      it "should make a POST request with correct params to add a webhook" do
+        sample_response = "{\"message\":\"Webhook has been created\",\"webhook\":{\"url\":\"http://postbin.heroku.com/860bcd65\"}}"
+        webhooks_url = @rubymail.webhooks.send(:webhook_url)
+        overwritten_url = 'http://rubymail.net/webhook'
+
+        expect(Rubymail).to receive(:submit).
+          with(:post, webhooks_url, {:id => @sample[:id], :url => overwritten_url}).
+          and_return(sample_response)
+
+        @rubymail.webhooks.create(@sample[:id], overwritten_url)
+      end
+    end
+  end
+
+  describe "update a webhook" do
+    context "using the default webhook url" do
+      it "should make a POST request with correct params to add a webhook" do
+        sample_response = "{\"message\":\"Webhook has been updated\",\"webhook\":{\"url\":\"http://postbin.heroku.com/860bcd65\"}}"
+        webhooks_url = @rubymail.webhooks.send(:webhook_url, @sample[:id])
+
+        expect(Rubymail).to receive(:submit).
+          with(:put, webhooks_url, {:url => @sample[:url]}).
+          and_return(sample_response)
+
+        @rubymail.webhooks.update(@sample[:id])
+      end
+    end
+    context "overwriting the default webhook url" do
+      it "should make a POST request with correct params to add a webhook" do
+        sample_response = "{\"message\":\"Webhook has been updated\",\"webhook\":{\"url\":\"http://postbin.heroku.com/860bcd65\"}}"
+        webhooks_url = @rubymail.webhooks.send(:webhook_url, @sample[:id])
+        overwritten_url = 'http://rubymail.net/webhook'
+
+        expect(Rubymail).to receive(:submit).
+          with(:put, webhooks_url, {:url => overwritten_url}).
+          and_return(sample_response)
+
+        @rubymail.webhooks.update(@sample[:id], overwritten_url)
+      end
+    end
+  end
+
+  describe "delete a webhook" do
+    it "should make a DELETE request with correct params" do
+      sample_response = "{\"message\":\"Webhook has been deleted\",\"webhook\":{\"url\":\"http://postbin.heroku.com/860bcd65\"}}"
+      webhooks_url = @rubymail.webhooks.send(:webhook_url, @sample[:id])
+
+      expect(Rubymail).to receive(:submit).
+        with(:delete, webhooks_url).
+        and_return(sample_response)
+
+      @rubymail.webhooks.delete(@sample[:id])
+    end
+  end
+end
