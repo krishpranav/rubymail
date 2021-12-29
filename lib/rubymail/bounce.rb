1d4c334c3a6c404ffd271571382831d720b4f4d8
krishpranav <krisna.pranav@gmail.com>
2021-12-29 19:37:30 +0530

    lib: rubymail bounce

--- lib/rubymail/bounce.rb
+++ lib/rubymail/bounce.rb
@@ -1,25 +1,31 @@
 module Rubymail
     class Bounce
+      def initialize(rubymail, domain)
+        @rubymail = rubymail
+        @domain  = domain
+      end
+      
+      def list(options={})
+        Rubymail.submit(:get, bounce_url, options)["items"] || []
+      end
 
-        def initialize(rubymail, domain)
-            @rubymail = rubymail
-            @domain = domain
-        end
-
-        def list(options={})
-            Rubymail.submit(:get, bounce_url, options)["items"] || []
-        end
-
-        def find(email)
-            Rubymail.submit :get, bounce_url(email)
-        end
-
-        def add(email, options={})
-            Rubymail.submit :post, bounce_url, {:address => email}.merge(options)
-        end
-
-        def destroy(email)
-            Rubymail.submit :delete, bounce_url(email)
-        end
+      def find(email)
+        Rubymail.submit :get, bounce_url(email)
+      end
+  
+      def add(email, options={})
+        Rubymail.submit :post, bounce_url, {:address => email}.merge(options)
+      end
+      
+      def destroy(email)
+        Rubymail.submit :delete, bounce_url(email)  
+      end
+  
+      private
+  
+      def bounce_url(address=nil)
+        "#{@rubymail.base_url}/#{@domain}/bounces#{'/' + address if address}"
+      end
+      
     end
-end
+  end
