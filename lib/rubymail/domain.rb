58776c16cb85df76a8ddc62ba85ed7ae119163fc
krishpranav <krisna.pranav@gmail.com>
2021-12-30 09:32:49 +0530

    lib: rubymail domain

--- lib/rubymail/domain.rb
+++ lib/rubymail/domain.rb
@@ -1,26 +1,34 @@
-module Rubymail 
+module Rubymail
     class Domain
-        def initialize(rubymail)
-            @rubymail = rubymail
-        end       
+      def initialize(rubymail)
+        @rubymail = rubymail
+      end
 
-        def list(options={})
-            Rubymail.submit(:get, domain_url, options)
-        end
-
-        def find(domain)
-            Rubymail.submit :get, domain_url(domain)
-        end
-
-        def create(domain, opts = {})
-            opts = {name: domain}.merge(opts)
-            Rubymail.submit :post, domain_url, opts
-        end
-
-        def verify(domain)
-            Rubymail.submit :put, "#{domain_url(domain)}/verify"
-        end
-        
-        end
+      def list(options={})
+        Rubymail.submit(:get, domain_url, options)["items"] || []
+      end
+  
+      def find(domain)
+        Rubymail.submit :get, domain_url(domain)
+      end
+  
+      def create(domain, opts = {})
+        opts = {name: domain}.merge(opts)
+        Rubymail.submit :post, domain_url, opts
+      end
+  
+      def delete(domain)
+        Rubymail.submit :delete, domain_url(domain)
+      end
+  
+      def verify(domain)
+        Rubymail.submit :put, "#{domain_url(domain)}/verify"
+      end
+  
+      private
+  
+      def domain_url(domain = nil)
+        "#{@rubymail.base_url}/domains#{'/' + domain if domain}"
+      end
     end
-end
+  end
