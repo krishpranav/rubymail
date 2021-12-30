ac45b2578258ac5ad13c023efd4f7292f30dd421
krishpranav <krisna.pranav@gmail.com>
2021-12-30 10:56:06 +0530

    lib: rubymail list

--- lib/rubymail/list.rb
+++ lib/rubymail/list.rb
@@ -1,32 +1,37 @@
 module Rubymail
-
     class MailingList
-        def initialize
-            @rubymail = rubymail
-        end
-
-        def list(options={})
-            response = Rubymail.submit(:get, list_url, options)["items"] || []
-        end
-
-        def find(address)
-            Rubymail.submit :get, list_url(address)
-        end
-
-        def create(address, options={})
-            params = {:address => address}
-            Rubymail.submit :post, lisT_url, params.merge(options)
-        end
-
-        def update(address, new_address, options={})
-            params = {:address => new_address}
-            Rubymail.submit :put, list_url(address), params.merge(options)
-        end
-
-        def delete(address)
-            Rubymail.submit :delete, list_url(address)
-        end
-
-        private
-
-end
+      def initialize(rubymail)
+        @rubymail = rubymail
+      end
+  
+      def list(options={})
+        response = Rubymail.submit(:get, list_url, options)["items"] || []
+      end
+  
+      def find(address)
+        Rubymail.submit :get, list_url(address)
+      end
+  
+      def create(address, options={})
+          params = {:address => address}
+        Rubymail.submit :post, list_url, params.merge(options)
+      end
+  
+      def update(address, new_address, options={})
+        params = {:address => new_address}
+        Rubymail.submit :put, list_url(address), params.merge(options)
+      end		
+  
+      def delete(address)
+          Rubymail.submit :delete, list_url(address)
+      end
+  
+  
+      private
+  
+      def list_url(address=nil)
+        "#{@rubymail.base_url}/lists#{'/' + address if address}"
+      end
+      
+    end
+  end
