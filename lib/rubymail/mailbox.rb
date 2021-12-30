2389b82290e7010aba5a350fcf5714301459f3d3
krishpranav <krisna.pranav@gmail.com>
2021-12-30 11:25:32 +0530

    lib: rubymail mailbox

--- lib/rubymail/mailbox.rb
+++ lib/rubymail/mailbox.rb
@@ -1,33 +1,41 @@
 module Rubymail
     class Mailbox
-        def initialize(rubymail, domain)
-            @rubymail = rubymail
-            @domain = domain
-        end
+  
+      def initialize(rubymail, domain)
+        @rubymail = rubymail
+        @domain  = domain
+      end
+      
+      def list(options={})
+        Rubymail.submit(:get, mailbox_url, options)["items"]
+      end
+      
+      def create(mailbox_name, password)
+        address = "#{mailbox_name}@#{@domain}"
+        Rubymail.submit(
+          :post,
+          mailbox_url,
+          {
+            :mailbox => address,
+            :password => password
+          }
+        )
+      end
+  
+      def update_password(mailbox_name, password)
+        Rubymail.submit :put, mailbox_url(mailbox_name), :password => password
+      end
+  
+      def destroy(mailbox_name)
+        Rubymail.submit :delete, mailbox_url(mailbox_name)
+      end
+  
+  
+      private
 
-        def list(options={})
-            Rubymail.submit(:get, mailbox_url, options)["items"]
-        end
-
-        def create(rubymail_name, password)
-            address = "#{mailbox_name}@#{domain}"
-            Rubymail.submit(
-                :post
-                mailbox_url,
-                {
-                    :mailbox => address,
-                    :password => password
-                }
-            )
-        end
-
-        def update_password(mailbox_name, password)
-            Rubymail.submit :put, mailbox_url(mailbox_url), :password => password
-        end
-        
-        def destroy(mailbox_name)
-            Rubymail.submit :delete, mailbox_url(mailbox_name)
-        end
-        
+      def mailbox_url(mailbox_name=nil)
+        "#{@rubymail.base_url}/#{@domain}/mailboxes#{'/' + mailbox_name if mailbox_name}"
+      end
+  
     end
-end
+  end
