7327a9ed31f0a25c2215bbe13fe2fd8331b93b02
krishpranav <krisna.pranav@gmail.com>
2021-12-30 11:38:50 +0530

    lib: rubymail error

--- lib/rubymail/rubymail_error.rb
+++ lib/rubymail/rubymail_error.rb
@@ -1,37 +1,53 @@
 module Rubymail
     class Error
-        attr_accessor :error
-
-        def initialize(options={})
-            @error =
-                case options[:code]
-                when 200
-                    # 200 status code[success status code]
-                when 404
-                    Rubymail::NotFound.new(options[:message])
-                when 400
-                    Rubymail::BadRequest.new(options[:message])
-                when 401
-                    Rubymail::Unauthorized.new(options[:message])
-                when 402
-                    Rubymail::RequestFailed.new(options[:message])
-                else
-                    Rubymail::ErrorBase.new(options[:message])
-                end
-            end
-
-        def hanndle
-            return error.handle
-        end
+      attr_accessor :error
+  
+      def initialize(options={})
+        @error =
+          case options[:code]
+          when 200
+            # 200 status code [success code]
+          when 404
+            Rubymail::NotFound.new(options[:message])
+          when 400
+            Rubymail::BadRequest.new(options[:message])
+          when 401
+            Rubymail::Unauthorized.new(options[:message])
+          when 402
+            Rubymail::ResquestFailed.new(options[:message])
+          when 500, 502, 503, 504
+            Rubymail::ServerError.new(options[:message])
+          else
+            Rubymail::ErrorBase.new(options[:message])
+          end
+      end
+  
+      def handle
+        return error.handle
+      end
     end
-    
+  
     class ErrorBase < StandardError
-        def handle
-            return self
-        end
-
-        class NotFound < ErrorBAase
+      def handle
+        return self
+      end
     end
-                
+  
+    class NotFound < ErrorBase
+      def handle
+        return nil
+      end
     end
-end
+  
+    class BadRequest < ErrorBase
+    end
+  
+    class Unauthorized < ErrorBase
+    end
+  
+    class ResquestFailed < ErrorBase
+    end
+  
+    class ServerError < ErrorBase
+    end
+  end
